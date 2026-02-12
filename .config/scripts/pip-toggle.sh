60666a60f66d0c2e9c8770f0233b327adc7655ed
ElisaAshryver <elisaashryver@hotmail.com>
2026-02-12 16:45:53 +0100

    update pip-toggle.sh to disable HDMI-A-1 and add delay before switching to single monitor mode

--- .config/scripts/pip-toggle.sh
+++ .config/scripts/pip-toggle.sh
@@ -7,6 +7,7 @@ if [[ "$MODE" == "5120x1440" ]]; then
   hyprctl keyword monitor DP-1,2560x1440@240.00,0x0,1
   hyprctl keyword monitor HDMI-A-1,2560x1440@240.00,2560x0,1
 else
+  hyprctl keyword monitor HDMI-A-1,disable
+  sleep 0.2
   hyprctl keyword monitor DP-1,5120x1440@239.76,0x0,1
-  hyprctl keyword monitor HDMI-A-1,5120x1440@239.76,5120x0,1
 fi
